3b7713a29052e941e720efd2a0a13aa8c82ebb6d
Ricky Primayuda Putra <113336156+rickyprimay@users.noreply.github.com>
2024-11-11 11:39:55 +0700

    Initial Commit

---
+++ ToDoListLearn/ToDoListLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListLearnApp.swift
+//  ToDoListLearn
+//
+//  Created by Ricky Primayuda Putra on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
